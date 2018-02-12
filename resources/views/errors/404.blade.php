4f9753f35542bf785410c0c007901cd19bfe87d0
Bruno Mota Cardoso <brunocardoso_2000@hotmail.com>
2018-02-12 22:03:57 +0100

    Custom error 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+@extends('errors::layout')
+
+@section('title', 'Page not found')
+@section('message')
+Sorry page not found. Contact an <a href="https://twitter.com/bruno_1226">administrator</a>
+@stop
