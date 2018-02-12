9fda8ee30a2f023c77c36c004f7ada3e7dbe1fc8
Bruno Mota Cardoso <brunocardoso_2000@hotmail.com>
2018-02-12 21:39:21 +0100

    Configure app name

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="text-center">
-	<p>&copy; {{ date('Y') }} &middot; Laracarte by <a href="https://twitter.com/bruno_1226">@bruno-1226</a> - A <a href="https://laramap.com/">Laramap</a> clone app.</p>
+	<p>&copy; {{ date('Y') }} &middot; {{config('app.name')}} by <a href="https://twitter.com/bruno_1226">@bruno-1226</a> - A <a href="https://laramap.com/">Laramap</a> clone app.</p>
 	<p><strong>This app has been built for learning purposes.</strong></p>
 </footer>
